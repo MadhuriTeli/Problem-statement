fecf93a5e61de2b843e34ae78c04afa5b42a6bc7
Madhuri <madhuriteli07@gmail.com>
2021-02-10 21:44:00 +0530

    combined

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,3 @@
-
 const statements = [{
     _id: '1',
     title: 'Reserchers of qualities throughout of reserch',
@@ -103,6 +102,15 @@ const statements = [{
 
 $(document).ready(function () {
 
+  function titleFunction(stat) {
+    if (stat == 1 || stat == 3 || stat == 4 || stat  == 6 || stat == 8 || stat == 10) {
+      return `<i class="fas fa-paperclip"></i>`;
+    }
+    else {
+      return '';
+    }
+  }
+
   function statusFunction(status) {
     if (status === 'Open') {
       return `<div class="alert alert-danger border-danger status" role="alert">Open</div>`;
@@ -113,23 +121,82 @@ $(document).ready(function () {
     }
   }
 
+  function criticalFunction(importance) {
+    if (importance === "Critical") {
+      return `<div class="critical-circle"></div>Critical`;
+    }
+    else {
+      return importance;
+    }
+  }
+
 
   const statementsElement = document.getElementById("statement-table");
   statements.forEach(statement => {
     const statementEle = document.createElement("tr");
-
+    const dropdownEle = document.createElement("div");
+    dropdownEle.innerHTML =`  <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Assign Project</a>
+            <a class="dropdown-item" href="#">Assign Contact</a>
+            <a class="dropdown-item" href="#">Add Notes</a>
+            <a class="dropdown-item" href="#">View Notes</a>
+            <a class="dropdown-item" href="#">Edit</a>
+            <a class="dropdown-item" href="#">View</a>
+            <a class="dropdown-item" href="#">copy</a>
+            <a class="dropdown-item" href="#">Delete</a>
+          </div>`
     statementEle.innerHTML =
 
 
-      `<td> <a href="#">${statement.title} </a></td>
+    `<td>${titleFunction(statement._id)}<a href="#"> ${statement.title}</a></td>
     <td> ${statement.createdBy} </td>
     <td> ${statement.category} </td>
-    <td> ${statement.importance} </td>
+    <td> ${criticalFunction(statement.importance)} </td>
     <td> ${statement.dueDate} </td>
     <td> ${statusFunction(statement.status)} </td>
-     <td><i class="fas fa-ellipsis-v"></i></td>
+
+  
+     <td>
+     <div class="dropdown">
+     <i class="fas fa-ellipsis-v "
+     type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+ </i><div class="dropdown-menu dropdown-menu-right">
+            <a class="dropdown-item" href="#">Assign Project</a>
+            <a class="dropdown-item" href="#">Assign Contact</a>
+            <a class="dropdown-item" href="#">Add Notes</a>
+            <a class="dropdown-item" href="#" >View Notes</a>
+            <a class="dropdown-item" href="#">Edit</a>
+            <a class="dropdown-item" href="#" id="openView">View</a>
+            <a class="dropdown-item" href="#">copy</a>
+            <a class="dropdown-item" href="#">Delete</a>
+          </div>
+          </div></td>
   `;
     statementsElement.appendChild(statementEle);
   });
+
+ $("#close").on("click", function () {
+    $("#problemStatement").hide();
+ });
+  
+   $("#openView").click(function () {
+    $("#problemStatement").show();
+  });
+
+   $("#open").click(function () {
+        $("#problemStatement").toggle();
+      });
+
+      var input = document.getElementById( 'files' );
+      var info = document.getElementById( 'filename' );
+
+      input.addEventListener( 'change', displayFileName );
+      function displayFileName( event ) {
+  
+        var inputfile = event.srcElement;
+        var fileName = inputfile.files[0].name;
+        console.log(inputfile);
+        info.textContent = ""+ fileName +"\n";
+      }
 
 });
